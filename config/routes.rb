5d83a9223b96bf6e7973f82a56ebfe3ca75659f4
matias596 <128436167+matias596@users.noreply.github.com>
2025-09-04 19:46:21 -0300

    feat: add authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resource :session
+  resources :passwords, param: :token
   namespace :api do
     resources :leaves, only: [:index, :create], as: :leaves
     resources :leaves, only: [:show, :update, :destroy], as: :leave
